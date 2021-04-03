e1a266ae15a1bec284fa1df969e4ef3c23edffee
veganum <veganum@hotmail.com>
2021-04-03 12:48:28 +0200

    subiendo proyecto

---
+++ src/main/java/com/boot/bookingrestaurantapi/BookingRestaurantApiApplication.java
@@ -0,0 +1,18 @@
+package com.boot.bookingrestaurantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingRestaurantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingRestaurantApiApplication.class, args);
+		System.out.println("___________________________________________________________");
+		System.out.println("|---------------------------------------------------------|");
+		System.out.println("|-----------------SERVIDOR CORRIENDO----------------------|");
+		System.out.println("|---------------------------------------------------------|");
+		System.out.println("___________________________________________________________");
+	}
+
+}
